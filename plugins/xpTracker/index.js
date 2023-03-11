982ab5c83068a1354935a8e813f26245dd99208d
Spryzu <spryzuofficial@gmail.com>
2023-03-11 15:07:39 -0600

    xpTracker pluggin first commit

--- plugins/xpTracker/index.js
+++ plugins/xpTracker/index.js
@@ -2,9 +2,25 @@ const { Client } = require('discord.js');
 const Bot = require('../../models/Bot');
 const Plugin = require('../../models/Plugin');
 
-const config = async(client = new Client()) =>
+const config = async(client = new Client(), { xpMessage, grow, restrictions }) =>
 {
     console.log(`Xp Tracker plugin configurated for ${client.user.tag}`);
+
+    const checkStrikes = (user, message) =>
+    {
+        user.strikes += 1;
+
+        if(user.strikes > restrictions.strikes.num) 
+        {
+            user.xp -= restrictions.strikes.xpPunish;
+
+            if(user.xp < 0) user.xp = 0;
+
+            user.strikes = 0;
+        }
+
+        user.lastMsg = message;
+    }
 
     const bot = await Bot.findOne({ botId: client.user.id });
     let plugin = await Plugin.findOne({ bot });
@@ -14,14 +30,17 @@ const config = async(client = new Client()) =>
         plugin = new Plugin({ bot });
     }
     
-    const currentData = plugin.data.filter(d => d.name === 'xpTracker')[0];
+    let currentData = plugin.data.filter(d => d.name === 'xpTracker')[0];
 
     if(!currentData)
     {
-        plugin.data.push({
+        const initialData = {
             name: 'xpTracker',
             users: []
-        });
+        };
+
+        currentData = initialData;
+        plugin.data.push(initialData);
     }
 
     await plugin.save();
@@ -34,16 +53,34 @@ const config = async(client = new Client()) =>
 
         if(!user)
         {
-            currentData.users.push({ id: message.author.id, xp: 0 });
+            currentData.users.push({ id: message.author.id, xp: 0, lv: 1, next: grow.initial, time: new Date().getTime() - restrictions.time, strikes: 0, lastMsg: '' });
         }
 
-        currentData.users.forEach(user =>
+        currentData.users.forEach(u =>
         {
-            if(user.id === message.author.id)
+            if(u.id === message.author.id)
             {
-                user.xp += 1;
+                const currentTime = new Date().getTime();
+
+                if((currentTime - u.time) < restrictions.time) return checkStrikes(u, message.content);
+                if(message.content.length < restrictions.minLength) return checkStrikes(u, message.content);
+                if(restrictions.repeated && (message.content === u.lastMsg)) return checkStrikes(u, message.content);
+                
+                u.xp += xpMessage;
+                
+                if(u.xp > u.next) u.lv += 1;
+
+                if(grow.type === 'linear') u.next = grow.initial * u.lv;
+
+                if(grow.type === 'cuadratic') u.next = grow.factor * Math.pow(u.lv, 2) - u.lv + grow.initial;
+
+                u.strikes = 0;
+                u.time = currentTime;
+                u.lastMsg = message.content;
             }
         });
+
+        console.log(currentData.users)
 
         plugin.data = plugin.data.filter(d => d.name !== 'xpTracker');
         plugin.data.push(currentData);
